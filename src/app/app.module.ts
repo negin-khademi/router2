dfc48a84d96577ccd591bfa9efb02989e0e84462
Negin <negin.khademi@gmail.com>
2023-03-10 13:53:24 +0330

    some changes in angular.json because of  generating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    HomeComponent,
+    NotFoundComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
